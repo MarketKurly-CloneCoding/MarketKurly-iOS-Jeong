4a0e6e8b31508b997686f1ec4207d8aa38483777
kwonjeong <kwonjeong9470@gmail.com>
2023-07-31 20:16:21 +0900

    [Setting] #1 - 에셋 추가

---
+++ MarketKurly-Clone/MarketKurly-Clone/Util/NameSpace/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  MarketKurly-Clone
+//
+//  Created by KJ on 2023/07/31.
+//
+
+import UIKit
+
+public enum Color {
+    
+    // MARK: - MainColor
+    
+    public static let mainColor = UIColor(named: "mainColor")!
+    public static let pointColor = UIColor(named: "pointColor")!
+    
+    // MARK: - Color
+    
+    public static let black = UIColor(named: "black")!
+    public static let discountColor = UIColor(named: "discountColor")!
+    public static let gray1 = UIColor(named: "gray1")!
+    public static let gray2 = UIColor(named: "gray2")!
+}
