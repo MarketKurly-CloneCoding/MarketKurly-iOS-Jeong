47d3992c66656ba129f5026f4f95babc78cfbf9b
kwonjeong <kwonjeong9470@gmail.com>
2023-07-31 20:28:00 +0900

    [Setting] #1 - TabBar 구현

--- MarketKurly-Clone/MarketKurly-Clone/Presentation/TabBar/TabBarController.swift
+++ MarketKurly-Clone/MarketKurly-Clone/Presentation/TabBar/TabBarController.swift
@@ -7,23 +7,59 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class TabBarController: UITabBarController {
+    
+    // MARK: - Properties
+    
+    private var tabs: [UIViewController] = []
+    
+    // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setTabBarItem()
+        setTabBarUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tabBar.frame.size.height = 48
     }
-    */
-
 }
+
+extension TabBarController {
+    
+    private func setTabBarItem() {
+        tabs = [
+            HomeViewController(),
+            CategoryViewController(),
+            SearchViewController(),
+            MypageViewController()
+        ]
+        
+        TabBarItemType.allCases.forEach {
+            tabs[$0.rawValue].tabBarItem = $0.setTabBarItem()
+            tabs[$0.rawValue].tabBarItem.tag = $0.rawValue
+        }
+        
+        setViewControllers(tabs, animated: false)
+    }
+    
+    private func setTabBarUI() {
+        UITabBar.clearShadow()
+        tabBar.tintColor = Color.mainColor
+        tabBar.layer.masksToBounds = false
+        let lineView = UIView(frame: CGRect(x: 0, y: 0, width: tabBar.frame.size.width, height: 1))
+        lineView.backgroundColor = .systemGray5
+        tabBar.addSubview(lineView)
+        let fontAttributes = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 10, weight: .regular)
+        ]
+        UITabBarItem.appearance().setTitleTextAttributes(fontAttributes, for: .normal)
+    }
+}
+
+
