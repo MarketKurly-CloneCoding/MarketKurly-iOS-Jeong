ec42e9d9231ea6ea8808eccca0fc76e935fd900f
kwonjeong <kwonjeong9470@gmail.com>
2023-08-02 19:39:26 +0900

    [Setting] #1 - 네비게이션 바 구현

--- MarketKurly-Clone/MarketKurly-Clone/Util/NameSpace/Color.swift
+++ MarketKurly-Clone/MarketKurly-Clone/Util/NameSpace/Color.swift
@@ -11,11 +11,12 @@ public enum Color {
     
     // MARK: - MainColor
     
-    public static let mainColor = UIColor(named: "mainColor")!
+    public static let mainPurple = UIColor(named: "mainColor")!
     public static let pointColor = UIColor(named: "pointColor")!
     
     // MARK: - Color
     
+    public static let white = UIColor(named: "white")!
     public static let black = UIColor(named: "black")!
     public static let discountColor = UIColor(named: "discountColor")!
     public static let gray1 = UIColor(named: "gray1")!
