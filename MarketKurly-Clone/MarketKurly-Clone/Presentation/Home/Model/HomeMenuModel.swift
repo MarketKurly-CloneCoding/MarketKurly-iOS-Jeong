78e7bad20636d4e6089d1a6349ede27a6e20b3f4
kwonjeong <kwonjeong9470@gmail.com>
2023-08-06 18:38:09 +0900

    [Feat] #3 - 홈 메뉴 타이틀 데이터바인드 함수 구현

--- MarketKurly-Clone/MarketKurly-Clone/Presentation/Home/Model/HomeMenuModel.swift
+++ MarketKurly-Clone/MarketKurly-Clone/Presentation/Home/Model/HomeMenuModel.swift
@@ -6,3 +6,20 @@
 //
 
 import UIKit
+
+struct HomeMenuModel {
+    let title: String
+}
+
+extension HomeMenuModel {
+    
+    static func homeMenuData() -> [HomeMenuModel] {
+        return [
+            HomeMenuModel(title: "컬리추천"),
+            HomeMenuModel(title: "신상품"),
+            HomeMenuModel(title: "베스트"),
+            HomeMenuModel(title: "알뜰쇼핑"),
+            HomeMenuModel(title: "특가/혜택")
+        ]
+    }
+}
